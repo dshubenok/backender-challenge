3aec799a0728b3b108fc81770b84f2d56e160653
Dmitriy Shubenok <dshubenok93@gmail.com>
2024-11-16 00:40:10 +0300

    Fix linting

--- src/event_outbox/models.py
+++ src/event_outbox/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class EventOutbox(models.Model):
     event_type = models.CharField(max_length=255)
     event_date_time = models.DateTimeField(default=timezone.now)
@@ -8,5 +9,5 @@ class EventOutbox(models.Model):
     event_context = models.JSONField()
     metadata_version = models.PositiveBigIntegerField(default=1)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.event_type} at {self.event_date_time}"
