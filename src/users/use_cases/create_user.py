fede319978299725ea75997ee837fd66b49aca26
Dmitriy Shubenok <dshubenok93@gmail.com>
2024-11-16 00:48:49 +0300

    Modify business logic to save events in EventOutbox

--- src/users/use_cases/create_user.py
+++ src/users/use_cases/create_user.py
@@ -3,9 +3,11 @@ from typing import Any
 import structlog
 
 from core.base_model import Model
-from core.event_log_client import EventLogClient
 from core.use_case import UseCase, UseCaseRequest, UseCaseResponse
 from users.models import User
+from event_outbox.models import EventOutbox
+from django.conf import settings
+from django.db import transaction
 
 logger = structlog.get_logger(__name__)
 
@@ -38,30 +40,30 @@ class CreateUser(UseCase):
     def _execute(self, request: CreateUserRequest) -> CreateUserResponse:
         logger.info('creating a new user')
 
-        user, created = User.objects.get_or_create(
-            email=request.email,
-            defaults={
-                'first_name': request.first_name, 'last_name': request.last_name,
-            },
-        )
-
-        if created:
-            logger.info('user has been created')
-            self._log(user)
-            return CreateUserResponse(result=user)
-
-        logger.error('unable to create a new user')
-        return CreateUserResponse(error='User with this email already exists')
-
-    def _log(self, user: User) -> None:
-        with EventLogClient.init() as client:
-            client.insert(
-                data=[
-                    UserCreated(
-                        email=user.email,
-                        first_name=user.first_name,
-                        last_name=user.last_name,
-                    ),
-                ],
+        with transaction.atomic():
+            user, created = User.objects.get_or_create(
+                email=request.email,
+                defaults={
+                    'first_name': request.first_name, 'last_name': request.last_name,
+                },
             )
 
+            if created:
+                logger.info('user has been created')
+                self._log(user)
+                return CreateUserResponse(result=user)
+
+            logger.error('unable to create a new user')
+            return CreateUserResponse(error='User with this email already exists')
+
+    def _log(self, user: User) -> None:
+        EventOutbox.objects.create(
+            event_type='user_created',
+            environment=settings.ENVIRONMENT,
+            event_context={
+                'email': user.email,
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+            },
+            metadata_version=1,
+        )
